f6f22b956601714d6835459a4213fb5fa82764b4
Jamaria Sims <75871993+JamariaSims@users.noreply.github.com>
2021-08-20 00:27:21 -0400

    done

--- client/src/components/EditMovieForm.js
+++ client/src/components/EditMovieForm.js
@@ -1,70 +1,123 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useHistory } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, useHistory } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-import axios from 'axios';
+import axios from "axios";
+import { MovieSharp } from "@material-ui/icons";
 
 const EditMovieForm = (props) => {
-	const { push } = useHistory();
+  const { push } = useHistory();
+  const { id } = useParams();
+  const [movie, setMovie] = useState({
+    title: "",
+    director: "",
+    genre: "",
+    metascore: 0,
+    description: "",
+  });
 
-	const [movie, setMovie] = useState({
-		title:"",
-		director: "",
-		genre: "",
-		metascore: 0,
-		description: ""
-	});
-	
-	const handleChange = (e) => {
-        setMovie({
-            ...movie,
-            [e.target.name]: e.target.value
-        });
-    }
+  const handleChange = (e) => {
+    e.preventDefault();
+    setMovie({
+      ...movie,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleSubmit = (e) => {
-		e.preventDefault();
-	}
-	
-	const { title, director, genre, metascore, description } = movie;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .put(`http://localhost:5000/api/movies/${id}`, movie)
+      .then((response) => {
+        setMovie(response.data);
+        push(`/api/movies/${id}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  const handleDelete = (e) => {
+    e.preventDefault();
+  };
 
-    return (
-	<div className="col">
-		<div className="modal-content">
-			<form onSubmit={handleSubmit}>
-				<div className="modal-header">						
-					<h4 className="modal-title">Editing <strong>{movie.title}</strong></h4>
-				</div>
-				<div className="modal-body">					
-					<div className="form-group">
-						<label>Title</label>
-						<input value={title} onChange={handleChange} name="title" type="text" className="form-control"/>
-					</div>
-					<div className="form-group">
-						<label>Director</label>
-						<input value={director} onChange={handleChange} name="director" type="text" className="form-control"/>
-					</div>
-					<div className="form-group">
-						<label>Genre</label>
-						<input value={genre} onChange={handleChange} name="genre" type="text" className="form-control"/>
-					</div>
-					<div className="form-group">
-						<label>Metascore</label>
-						<input value={metascore} onChange={handleChange} name="metascore" type="number" className="form-control"/>
-					</div>		
-					<div className="form-group">
-						<label>Description</label>
-						<textarea value={description} onChange={handleChange} name="description" className="form-control"></textarea>
-					</div>
-									
-				</div>
-				<div className="modal-footer">			    
-					<input type="submit" className="btn btn-info" value="Save"/>
-					<Link to={`/movies/1`}><input type="button" className="btn btn-default" value="Cancel"/></Link>
-				</div>
-			</form>
-		</div>
-	</div>);
-}
+  useEffect(() => {
+    axios.get(`http://localhost:5000/api/movies/${id}`).then((response) => {
+      localStorage.setItem(`Movie${id}`, JSON.stringify(response.data));
+      return setMovie(response.data);
+    });
+  }, []);
+  const { title, director, genre, metascore, description } = movie;
+
+  return (
+    <div className="col">
+      <div className="modal-content">
+        <form onSubmit={handleSubmit}>
+          <div className="modal-header">
+            <h4 className="modal-title">
+              Editing <strong>{movie.title}</strong>
+            </h4>
+          </div>
+          <div className="modal-body">
+            <div className="form-group">
+              <label>Title</label>
+              <input
+                value={title}
+                onChange={handleChange}
+                name="title"
+                type="text"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label>Director</label>
+              <input
+                value={director}
+                onChange={handleChange}
+                name="director"
+                type="text"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label>Genre</label>
+              <input
+                value={genre}
+                onChange={handleChange}
+                name="genre"
+                type="text"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label>Metascore</label>
+              <input
+                value={metascore}
+                onChange={handleChange}
+                name="metascore"
+                type="number"
+                className="form-control"
+              />
+            </div>
+            <div className="form-group">
+              <label>Description</label>
+              <textarea
+                value={description}
+                onChange={handleChange}
+                name="description"
+                className="form-control"
+              ></textarea>
+            </div>
+          </div>
+          <div className="modal-footer">
+            <input type="submit" className="btn btn-info" value="Save" />
+            <Link to={`/movies/${id}`}>
+              <input type="button" className="btn btn-default" value="Cancel" />
+            </Link>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default EditMovieForm;
